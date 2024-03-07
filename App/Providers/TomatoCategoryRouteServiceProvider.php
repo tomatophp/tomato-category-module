4401a164784778f6eca7c43830f3edcbc3f28b95
Fady Mondy <info@3x1.io>
2024-03-07 17:59:13 +0200

    first commit 🔥

---
+++ App/Providers/TomatoCategoryRouteServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\TomatoCategory\App\Providers;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use TomatoPHP\TomatoAdmin\Facade\TomatoMenu;
+use TomatoPHP\TomatoAdmin\Services\Contracts\Menu;
+use Modules\TomatoCategory\App\Services\TomatoCategoryHandler;
+use Modules\TomatoCategory\App\Services\CategoryServices;
+
+
+class TomatoCategoryRouteServiceProvider extends RouteServiceProvider
+{
+    public function boot(): void
+    {
+        $this->routes(function (){
+            \Modules\TomatoCategory\App\Services\CategoryServices::loadRoutes();
+        });
+    }
+
+}
